b705a0e749d04eef8d2add212e76c50a55ea8d80
H-Cavid <cavid.hasanov2@bk.ru>
2021-04-06 13:36:04 +0400

    Task of Lesson

--- task06.py
+++ task06.py
@@ -1,3 +1,14 @@
 # sort methodundan istifade olunacaq 
 # sort()elifba sirasi ve ya artan sira ile gosterir
 # sort(reverse=True) elifba sirasinin eksi,azalan sira ile gosterecek
+L = [3, 6, 7, 4, -5, 4, 3, -1]
+
+#1.
+if sum(L)>2:
+    print(len(L))
+
+#2.
+if abs(max(L)-min(L))>10:
+    print(sorted(L))
+else: 
+    print("Ferq 10-dan kichik-beraberdir.")
